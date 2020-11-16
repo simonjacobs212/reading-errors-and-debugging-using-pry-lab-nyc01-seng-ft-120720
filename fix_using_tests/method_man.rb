cb993cfbf4bdc8583a7fa3b1f90dfe0ba2c5e2a5
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-16 17:06:58 +0000

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,11 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  greeting = "Hello #{player1} & #{player2}"
+  greeting
 end
 
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+def play_game(player2, player1)
+  message = "#{player2} is better than #{player1}"
+  message
 end
