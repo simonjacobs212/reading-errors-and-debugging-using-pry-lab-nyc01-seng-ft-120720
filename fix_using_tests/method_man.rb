6c0c9275b219cb5a0debba457a838546c0c6fa87
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-16 15:27:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,8 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  puts "Hello #{player1} & #{player2}"
+  greeting = "Hello #{player1} & #{player2}"
+  greeting
 end
 
 def play_game(player2)
