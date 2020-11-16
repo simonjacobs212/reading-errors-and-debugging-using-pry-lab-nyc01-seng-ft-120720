31c3e0401d703a8384658977d8578b24708422f0
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-16 16:34:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,12 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    string2 = "s" * 10 + string
-    string2
+    "s" * 10 + string
   else
     string
   end
 end
+
+snake_it_up("surprise!")
